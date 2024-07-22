0b317e41f216d0405a34904f009c0f85441749b3
Hassan Murtaza <hassan.murtaza@devsinc.com>
2024-07-22 15:14:21 +0500

    Added 'add new employee' feature

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,8 +3,22 @@ class AdminsController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(employee_params)
+
+    if @user.persisted?
+      redirect_to root_path, notice: "Employee successfully registered."
+    else
+      render :new 
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :user_type)
   end
 end
