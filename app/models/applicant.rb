94e769602c38e8526a3846c80ab5e047fbf291f7
Hassan Murtaza <hassan.murtaza@devsinc.com>
2024-07-30 20:53:31 +0500

    Added validation on cnic and email of model applicant and removed one logic error

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -4,6 +4,8 @@ class Applicant < ApplicationRecord
   has_many :jobs, through: :job_applicants
   has_one_attached :resume
 
-  validates :name, :email, :cnic, :linkedin_profile, presence: true
+  validates :name, :linkedin_profile, presence: true
   validates :resume, attached: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :cnic, presence: true, format: { with: /\A\d{5}-\d{7}-\d\z/ }
 end
