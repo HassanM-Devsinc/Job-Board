b9b2e3e265f6cc26f722ab4d9f06dffbd1c09679
Hassan Murtaza <hassan.murtaza@devsinc.com>
2024-07-31 01:18:13 +0500

    Added email format validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
 
   enum :user_type, { job_seeker: 0, admin: 1, employer: 2}
 end
