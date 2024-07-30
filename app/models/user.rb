f6f55fb81a2b197207a6fdf638e1cbbccdbf64f0
Hassan Murtaza <hassan.murtaza@devsinc.com>
2024-07-31 01:22:26 +0500

    Removed extra line

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,5 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
-
   enum :user_type, { job_seeker: 0, admin: 1, employer: 2}
 end
