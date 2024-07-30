be576de8de5b51f3209c910c1238ad824cb34437
Hassan Murtaza <hassan.murtaza@devsinc.com>
2024-07-30 21:54:20 +0500

    Added validations on resume and linkedin_profile of model applicant

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -4,8 +4,9 @@ class Applicant < ApplicationRecord
   has_many :jobs, through: :job_applicants
   has_one_attached :resume
 
-  validates :name, :linkedin_profile, presence: true
-  validates :resume, attached: true
+  validates :name, presence: true
+  validates :resume, attached: true, content_type: { in: 'application/pdf'}, size: { less_than: 5.megabytes}
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :cnic, presence: true, format: { with: /\A\d{5}-\d{7}-\d\z/ }
+  validates :linkedin_profile, presence: true, format: { with: /\A(https?:\/\/)?(www\.)?linkedin\.com\/(in\/[\w-]+|company\/[\w-]+|pub\/[\w-]+\/[\w-]+\/[\w-]+)?\z/ }
 end
